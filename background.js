5728e59b95a4b42b85abfebd89d465615890b793
Nathan Klair <nklair@nd.edu>
2016-10-01 13:09:09 -0400

    fixed it

--- background.js
+++ background.js
@@ -1,4 +1,45 @@
 var timer = window.setInterval( function() {alert("This is an alert")}, 60000);
 clearIntverval(timer);
 
+function injectCode() {
+
+	var body = document.body;
+
+	var html = 
+/*	
+	var head = document.head;
+
+	var link = document.createElement(LINK);
+	link.setAttribute('href', )
+
+	var outerDiv = document.createElement("DIV");
+	outerDiv.className = "modal";
+	outerDiv.id = "myModal";
+
+	var innerDiv = document.createElement("DIV");
+	innerDiv.className = "modal-content";
+
+	var close = document.createElement("SPAN");
+	close.className = "close";
+	close.innerHTML = "Close";
+	
+	var next = document.createElement("SPAN");
+	next.className = "next";
+	next.innerHTML = "Next";
+
+	var center = document.createElement("CENTER");
+
+	var para = document.createElement("P");
+	para.className = "text";
+	
+	center.appendChild(para);
+	innerDiv.appendChild(close);
+	innerDiv.appendChild(next);
+	innerDiv.appendChild(center);
+	outerDiv.appendChild(innerDiv);
+*/
+
+
+}
+
 
